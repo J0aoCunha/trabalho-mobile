f1b86110a3fd7b1c4fc7985e04f4ac76ad35e6e7
J0aoCunha <jvjunger20123@gmail.com>
2024-11-18 23:31:35 -0300

    removendo chaves

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js"
 import { AppState } from "react-native"
 import "react-native-url-polyfill"
 
-const supabaseUrl = "https://mbtqagauddxemkwmntai.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1idHFhZ2F1ZGR4ZW1rd21udGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4OTc4MjYsImV4cCI6MjA0NzQ3MzgyNn0.RBVD90iJujXcMl0_Preh6wy4YSgPJjAlV6gbhy33T04"
+const supabaseUrl = ""
+const supabaseAnonKey = ""
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
